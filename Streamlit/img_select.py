0eac4ade8db83f7c396bc35ac8789fd6288e92d3
SeMin <114004065+semsemin@users.noreply.github.com>
2024-11-24 18:45:57 +0900

    feature/#34 - summary

--- Streamlit/img_select.py
+++ Streamlit/img_select.py
@@ -2,8 +2,14 @@ import streamlit as st
 from PIL import Image
 import bring_img
 import gpt
+from product_data import fetch_online_product_data
+from st_sort import display_sorted_products  # st_sort.py에서 display_sorted_products 가져오기
 from image_cropper import crop_and_save_image  # 이미지 크롭 및 저장 기능
 from recommand_algo import generate_content_based_recommendations  # recommand_algo.py에서 추천 알고리즘 함수 가져오기
+from display_total_recommendation import display_recommendations  # display_total_recommendation.py에서 결과 출력 함수 가져오기
+from Ganpan_demo.menu_recom import recommend_best_menus
+
+
 
 def img_select(task_type):
     st.title("ItemInsight")
@@ -23,24 +29,30 @@ def img_select(task_type):
             # GPT로 데이터 추출
             if task_type == 'label':
                 result = gpt.get_gpt_response(cropped_image_path)  # 상품 정보 추출
-                st.write("GPT에서 추출한 데이터 (원본 결과):")
+                st.write("GPT에서 추출한 데이터 :")
                 st.json(result)  # 원본 데이터 확인
                 
                 if result:
                     # "product name" 키를 사용해 product_data 생성
-                    product_data = [{"product_name": item["product name"]} for item in result]
+                    product_data = [{"product_name": item["product_name"]} for item in result]
 
                     # 상세 데이터 가져오기
                     detailed_data = fetch_online_product_data(product_data)
                     
                     # 정렬 및 표시를 위해 session_state에 저장
                     st.session_state["product_data"] = detailed_data
-                    st.write("데이터가 정렬 및 표시될 준비가 완료되었습니다.")
-                    
+                    local_data = result
+
                     # display_sorted_products 호출
                     display_sorted_products()
-                    recommendation_df, recommendations = generate_content_based_recommendations(result, detailed_data)
+                    recommendation_df, recommendations = generate_content_based_recommendations(local_data, detailed_data)
                     display_recommendations(recommendation_df, recommendations)
 
             elif task_type == 'ganpan':
                 result = gpt.get_product_data(cropped_image_path)  # 매장 정보 추출
+                # result 확인
+                st.write("GPT에서 추출한 데이터 :", result)
+
+                best_menu = recommend_best_menus(result)
+                st.write("베스트 메뉴 추천 :")
+                st.json(best_menu)
